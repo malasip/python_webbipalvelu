137433e9868594451a9767fa74db4fc3e46611e4
malasip <alasippolamika@gmail.com>
2021-05-24 15:18:14 +0300

    Added gitignore

--- 01-hello-flask/hello-flask.py
+++ 01-hello-flask/hello-flask.py
@@ -8,10 +8,10 @@ def index():
     content = 'Hello, world!'
     return render_template('hello.html', title = 'Hello', content = content)
 
-@app.route("/secret")
+@app.route('/secret')
 def secret():
     content = 'You found a secret'
     return render_template('hello.html', title = 'Much secret', content = content)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
