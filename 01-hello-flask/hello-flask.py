a18ddcf7010f0ce68995de63f22e0bb2323db3ca
Mika Ala-Sippola <alasippolamika@gmail.com>
2021-05-24 13:42:02 +0300

    Update hello-flask.py
    Fixed typo

--- 01-hello-flask/hello-flask.py
+++ 01-hello-flask/hello-flask.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     content = 'Hello, world!'
-    return render_template('hello.htlm', title = 'Hello', content = content)
+    return render_template('hello.html', title = 'Hello', content = content)
 
 @app.route("/secret")
 def secret():
     content = 'You found a secret'
-    return render_template('hello.htlm', title = 'Much secret', content = content)
+    return render_template('hello.html', title = 'Much secret', content = content)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
