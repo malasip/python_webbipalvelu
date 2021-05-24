111e113dcbed6a84850e2744c33961eebe3cea1a
Mika Ala-Sippola <alasippolamika@gmail.com>
2021-05-24 13:34:17 +0300

    Templates, wooooo

--- 01-hello-flask/hello-flask.py
+++ 01-hello-flask/hello-flask.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello, world!"
+    content = 'Hello, world!'
+    return render_template('hello.htlm', title = 'Hello', content = content)
 
 @app.route("/secret")
 def secret():
-    return "You found a secret"
+    content = 'You found a secret'
+    return render_template('hello.htlm', title = 'Much secret', content = content)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
