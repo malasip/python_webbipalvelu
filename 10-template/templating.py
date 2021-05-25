cd92c50e81c74c02d90287783ea668f564d3bc4e
malasip <alasippolamika@gmail.com>
2021-05-25 09:37:57 +0300

    template demos

--- 10-template/templating.py
+++ 10-template/templating.py
@@ -8,5 +8,11 @@ def index():
     content = 'Document content'
     return render_template('base.html', title = title, content = content)
 
+@app.route('/about')
+def about():
+    title = 'About page'
+    content = 'About page with larger font'
+    return render_template('about.html', title = title, content = content)
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python_webbipalvelu
 Python weppipalvelu - ideasta tuotantoon (2021 alkukesä) ICT8TN034-3001 kurssin tehtävät
+Kaikki kurssin tehtävät tulevat osoitteesta https://terokarvinen.com/2021/python-web-service-from-idea-to-production/
