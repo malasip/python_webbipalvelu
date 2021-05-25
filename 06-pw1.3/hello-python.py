5782b511a929c5ba265b0edc8f4fb330b5432761
malasip <alasippolamika@gmail.com>
2021-05-25 08:00:25 +0300

    Fix

--- 06-pw1.3/hello-python.py
+++ 06-pw1.3/hello-python.py
@@ -1,2 +1,12 @@
 #!/bin/python3
 
+import sys
+
+def main(name):
+    print(f'Hello { name }')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print('Usage: hello-python.py <name>')
