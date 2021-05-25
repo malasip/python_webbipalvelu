43e15622d113660f47d3667c118d6b478c01cbb1
malasip <alasippolamika@gmail.com>
2021-05-25 09:22:31 +0300

    content patch 1

--- 10-template/templating.py
+++ 10-template/templating.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    title = 'Document title'
+    content = 'Document content'
+    return render_template('base.html', title = title, content = content)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
