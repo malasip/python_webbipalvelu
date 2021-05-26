a75ceb9f04247d877b565acc580f2b57a28ed122
malasip <alasippolamika@gmail.com>
2021-05-26 12:37:41 +0300

    flask-db-field-orm

--- 23-flask-db-orm-form/flask-db.py
+++ 23-flask-db-orm-form/flask-db.py
@@ -2,8 +2,11 @@ from datetime import datetime
 from flask import Flask
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.orm import model_form
 
 app = Flask(__name__)
+app.secret_key = 'gufds098ghou4an a9fasgf89afg89aseasf'
 
 db = SQLAlchemy(app)
 
@@ -17,6 +20,8 @@ class Devtype(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False)
     devices = db.relationship('Device', backref=db.backref('devtype', lazy=True))
+
+DeviceForm = model_form(model = Device, base_class = FlaskForm, db_session = db.session)
 
 @app.before_first_request
 def initDB():
@@ -34,6 +39,12 @@ def index():
     devices = Device.query.all()
     title = 'Simple database with relations'
     return render_template('index.html', title = title, devices = devices)
+
+@app.route('/add', methods = ['GET', 'POST'])
+def add():
+    title = 'Add new device'
+    form = DeviceForm()
+    return render_template('new.html', title = title, form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
