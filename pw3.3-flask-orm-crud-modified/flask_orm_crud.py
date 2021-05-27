f04120dbd6102e542c2fa57303c5c5b206b81d84
malasip <alasippolamika@gmail.com>
2021-05-27 14:13:14 +0300

    Modified version of PW3.3 hw

---
+++ pw3.3-flask-orm-crud-modified/flask_orm_crud.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from flask import Flask, render_template, redirect, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.orm import model_form
+from secrets import SystemRandom
+from markupsafe import escape
+
+
+app = Flask(__name__)
+app.secret_key = SystemRandom.randbytes(SystemRandom, 128)
+
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user = db.Column(db.String, nullable = False)
+    title = db.Column(db.String, nullable = False)
+    content = db.Column(db.String, nullable = False)
+    timestamp = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
+    modified = db.Column(db.DateTime)
+    parent_id = db.Column(db.Integer, db.ForeignKey('message.id'))
+    replies = db.relationship('Message', backref = db.backref('parent', remote_side = [id]))
+
+NewMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['replies', 'parent', 'timestamp', 'modified'])
+EditMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user', 'replies', 'parent', 'timestamp', 'modified'])
+ReplyMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['title', 'replies', 'parent', 'timestamp', 'modified'])
+
+@app.before_first_request
+class initDB():
+    db.create_all()
+
+@app.route('/')
+def index():
+    title = 'All messages'
+    messages = Message.query.filter(Message.parent_id == None).all()
+    return render_template('index.html', title = title, messages = messages)
+
+
+@app.route('/reply/<int:id>', methods = ['GET', 'POST'])
+@app.route('/add', methods = ['GET', 'POST'])
+def add(id = None):
+    if id:
+        form = ReplyMessageForm()
+    else:
+        form = NewMessageForm()
+    if form.validate_on_submit():
+        message = Message()
+        form.populate_obj(message)
+        if id:
+            message.parent_id = id
+            message.title = f'Re: {Message.query.get_or_404(id).title}'
+        db.session.add(message)
+        db.session.commit()
+        flash('Message posted succesfully')
+        return redirect('/')
+    title = 'Post a new message'
+    return render_template('add_form.html', title = title, form = form)
+
+@app.route('/edit/<int:id>', methods = ['GET', 'POST'])
+def edit(id = None):
+    message = Message.query.get_or_404(id)
+    form = EditMessageForm(obj = message)
+    if(form.validate_on_submit()):
+        form.populate_obj(message)
+        message.modified = datetime.utcnow()
+        db.session.commit()
+        flash('Message modified succesfully')
+        return redirect('/')
+    title = f'Editing message "{ message.title }"'
+    return render_template('edit_form.html', title = title, form = form, id = id)
+
+@app.route('/delete/<int:id>')
+def delete(id = None):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    flash('Message deleted succesfully')
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
