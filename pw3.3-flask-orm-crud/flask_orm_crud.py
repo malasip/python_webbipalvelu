cd6c3143248c9e77be7fbd23d0300be359a3ab26
malasip <alasippolamika@gmail.com>
2021-05-27 10:30:17 +0300

    Modified reply system

--- pw3.3-flask-orm-crud/flask_orm_crud.py
+++ pw3.3-flask-orm-crud/flask_orm_crud.py
@@ -8,7 +8,7 @@ from markupsafe import escape
 
 
 app = Flask(__name__)
-app.secret_key = SystemRandom.randbytes(SystemError, 128)
+app.secret_key = SystemRandom.randbytes(SystemRandom, 128)
 
 db = SQLAlchemy(app)
 
@@ -17,13 +17,14 @@ class Message(db.Model):
     user = db.Column(db.String, nullable = False)
     title = db.Column(db.String, nullable = False)
     content = db.Column(db.String, nullable = False)
-    timestamp = db.Column(db.DateTime, nullable = False, default = datetime.now())
+    timestamp = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
     modified = db.Column(db.DateTime)
     parent_id = db.Column(db.Integer, db.ForeignKey('message.id'))
     replies = db.relationship('Message', backref = db.backref('parent', remote_side = [id]))
 
 NewMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['replies', 'parent', 'timestamp', 'modified'])
 EditMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user', 'replies', 'parent', 'timestamp', 'modified'])
+ReplyMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['title', 'replies', 'parent', 'timestamp', 'modified'])
 
 @app.before_first_request
 class initDB():
@@ -32,15 +33,22 @@ class initDB():
 @app.route('/')
 def index():
     title = 'All messages'
-    messages = Message.query.all()
+    messages = Message.query.filter(Message.parent_id == None).all()
     return render_template('index.html', title = title, messages = messages)
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
-    form = NewMessageForm()
+    if not request.args.get('reply'):
+        form = NewMessageForm()
+    else:
+        form = ReplyMessageForm()
     if(form.validate_on_submit()):
         message = Message()
         form.populate_obj(message)
+        if request.args.get('reply'):
+            parent_id = int(request.args.get('reply'))
+            message.parent_id = parent_id
+            message.title = f'Re: {Message.query.get(parent_id).title}'
         db.session.add(message)
         db.session.commit()
         flash('Message posted succesfully')
@@ -54,9 +62,8 @@ def edit():
     message = Message.query.get(id)
     form = EditMessageForm(obj = message)
     if(form.validate_on_submit()):
-        message.title = request.form.get('title')
-        message.content = request.form.get('content')
-        message.modified = datetime.now()
+        form.populate_obj(message)
+        message.modified = datetime.utcnow()
         db.session.commit()
         flash('Message modified succesfully')
         return redirect('/')
