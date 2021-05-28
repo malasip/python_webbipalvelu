f4ee094b506529753bbcb5014944eca235d4adf0
malasip <alasippolamika@gmail.com>
2021-05-28 15:54:51 +0300

    Updates

--- flask-message-board/README.md
+++ flask-message-board/README.md
@@ -1,5 +1,11 @@
 # Simple flask message board
 
-Simple message board app made in flask
+Simple message board app made in flask.  
+
+Documentation sources used for project:  
+* https://jinja.palletsprojects.com/en/2.11.x/api/
+* https://terokarvinen.com//2016/flask-templates/index.html
+* https://flask-sqlalchemy.palletsprojects.com/en/2.x/
+* https://developer.mozilla.org/en-US/docs/Web/CSS/:target
 
 ![alt text](screenshot.png "Screenshot")
--- flask-message-board/flask-message-board.py
+++ flask-message-board/flask-message-board.py
@@ -1,11 +1,13 @@
 from datetime import datetime
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, validators
 from wtforms.ext.sqlalchemy.orm import model_form
 from secrets import SystemRandom
 from markupsafe import escape
 from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, TextAreaField, validators
+from sqlalchemy import exc
 
 
 app = Flask(__name__)
@@ -23,82 +25,189 @@ class Message(db.Model):
     parent_id = db.Column(db.Integer, db.ForeignKey('message.id'))
     replies = db.relationship('Message')
 
+NewMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user_id', 'user', 'replies', 'parent', 'timestamp', 'modified', 'thread_id'])
+NoTitleMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user_id', 'user', 'title', 'replies', 'parent', 'timestamp', 'modified', 'thread_id'])
+# Message creation/ edit forms
+class NewMessageForm(FlaskForm):
+    title = StringField('Title', validators = [validators.InputRequired()])
+    message = TextAreaField('Message', validators = [validators.InputRequired()])
+
+class NoTitleMessageForm(FlaskForm):
+    message = TextAreaField('Message', validators = [validators.InputRequired()])
+
+
+
+# User model and functions
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, nullable = False)
-    displayname = db.Column(db.String, nullable = False)
-    password = db.Column(db.String, nullable = False)
-    active = db.Column(db.DateTime)
+    username = db.Column(db.String, nullable = False, unique = True)
+    displayname = db.Column(db.String, nullable = False, unique = True)
+    pwhash = db.Column(db.String, nullable = False)
+    active = db.Column(db.DateTime, default = datetime.utcnow())
+    moderator = db.Column(db.Boolean, nullable = False, default = False)
     messages = db.relationship('Message', backref = db.backref('user', remote_side = [id]))
 
     def setPassword(self, password):
-        self.password = generate_password_hash(password)
+        self.pwhash = generate_password_hash(password)
 
     def checkPassword(self, password):
-        return check_password_hash(self.password, password)
-        
+        return check_password_hash(self.pwhash, password)
 
-NewMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user_id', 'user', 'replies', 'parent', 'timestamp', 'modified'])
-EditMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user_id', 'user', 'title', 'replies', 'parent', 'timestamp', 'modified'])
-ReplyMessageForm = model_form(Message, db_session = db.session, base_class = FlaskForm, exclude = ['user_id', 'user', 'title', 'replies', 'parent', 'timestamp', 'modified'])
+def currentUser():
+    try:
+        uid = int(session['uid'])
+    except:
+        return None
+    return User.query.get_or_404(uid)
 
+# Set currentUser function as global for Jinja
+app.jinja_env.globals['currentUser'] = currentUser
+
+# User login and creation/ edit forms
+class UserForm(FlaskForm):
+    username = StringField('Username', validators = [validators.InputRequired()])
+    password = PasswordField('Password', validators = [validators.InputRequired()])
+    displayname = StringField('Display name', validators = [validators.InputRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators = [validators.InputRequired()])
+    password = PasswordField('Password', validators = [validators.InputRequired()])
+
+# Initialize database
 @app.before_first_request
 class initDB():
     db.create_all()
-    user = User(username = 'Mika', displayname = 'Postaaja', active = datetime.utcnow())
-    user.setPassword('kala')
+    user = User(username = 'Moderator', displayname = 'Moderator', moderator = True)
+    user.setPassword('Moderator')
     db.session.add(user)
     db.session.commit()
-    message = Message(user_id = user.id, title = 'test', message = 'Test message')
+    message = Message(user_id = user.id, title = 'First post', message = 'This is a first post')
     db.session.add(message)
     db.session.commit()
 
+@app.route('/user/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter(User.username == username).first()
+        if not user:
+            flash('Login failed, bad username or password')
+            return redirect('/user/login')
+        if user.checkPassword(password) and user.active != None:
+            session['uid'] = user.id
+            flash('Login successfull')
+            url = request.args.get('r')
+            if not url:
+                url = '/'
+            return redirect(url)
+        flash('Login failed, bad username or password')
+        return redirect('/user/login')
+    return render_template('form.html', form = form)
+
+@app.route('/user/register', methods = ['GET', 'POST'])
+def register():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, displayname = form.displayname.data)
+        user.setPassword(form.password.data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash('User created successfully.')
+        except exc.IntegrityError:
+            db.session.rollback()
+            flash('Username not avalable')
+            return redirect('/user/register')
+        session['uid'] = user.id
+        url = request.args.get('r')
+        if not url:
+            url = '/'
+        return redirect(url)
+    return render_template('form.html', form = form)
+
+@app.route('/user/logout')
+def logout():
+    session['uid'] = None
+    flash('Logged out successfully')
+    return redirect('/')
+
+@app.route('/<int:id>')
 @app.route('/')
-def index():
-    title = 'All threads'
+def index(id = None):
+    if id:
+        message = Message.query.get_or_404(id)
+        messages = [message]
+        return render_template('index.html', messages = messages)
     messages = Message.query.filter(Message.parent_id == None).order_by(Message.id.desc()).all()
-    return render_template('index.html', title = title, messages = messages)
+    return render_template('index.html', messages = messages)
 
 @app.route('/<int:id>/reply', methods = ['GET', 'POST'])
 @app.route('/add', methods = ['GET', 'POST'])
 def add(id = None):
+    if currentUser() == None:
+        flash('This action requires valid user')
+        return redirect('/')
     if id:
-        form = ReplyMessageForm()
+        form = NoTitleMessageForm()
     else:
         form = NewMessageForm()
     if form.validate_on_submit():
         message = Message()
         form.populate_obj(message)
+        message.user_id = currentUser().id
+        url = '/'
         if id:
             message.parent_id = id
             message.title = f'Re: {Message.query.get_or_404(id).title}'
-        db.session.add(message)
-        db.session.commit()
-        flash('Message posted succesfully')
-        return redirect('/')
-    title = 'Post a new message'
-    return render_template('form.html', title = title, form = form)
+            url = request.args.get('r')
+        try:
+            db.session.add(message)
+            db.session.commit()
+            flash('Message posted succesfully')
+            return redirect(url)
+        except:
+            db.session.rollback()
+            flash('Something went wrong')
+            return redirect(url)
+    return render_template('form.html', form = form)
 
 @app.route('/<int:id>/edit', methods = ['GET', 'POST'])
 def edit(id = None):
+    if currentUser() == None:
+        flash('This action requires valid user')
+        return redirect('/')
     message = Message.query.get_or_404(id)
-    form = EditMessageForm(obj = message)
+    if message.user_id != currentUser().id or not currentUser().moderator:
+        flash('You do not have permission to modify this message')
+        return redirect('/')
+    form = NoTitleMessageForm(obj = message)
     if(form.validate_on_submit()):
         form.populate_obj(message)
         message.modified = datetime.utcnow()
-        db.session.commit()
-        flash('Message modified succesfully')
-        return redirect('/')
-    title = f'Editing message "{ message.title }"'
-    return render_template('form.html', title = title, form = form, id = id)
+        try:
+            db.session.commit()
+            flash('Message modified succesfully')
+        except:
+            db.session.rollback()
+            flash('Something went wrong')
+        url = request.args.get('r')
+        return redirect(url)
+    #title = f'Editing message "{ message.title }"'
+    return render_template('form.html', form = form, id = id)
 
 @app.route('/<int:id>/delete')
 def delete(id = None):
+    if currentUser() == None:
+        flash('This action requires valid user')
+        return redirect('/')
     message = Message.query.get_or_404(id)
     db.session.delete(message)
     db.session.commit()
     flash('Message deleted succesfully')
-    return redirect('/')
+    url = request.args.get('r')
+    return redirect(url)
 
 
 if __name__ == '__main__':
